194f94811b18c05e013e2447aa3f667e82211a08
johannpereze <sebas.jsp@gmail.com>
2021-09-15 19:51:38 -0500

    Clase 32. ejemplo con mala practica. En el próximo commit usamos una buena practica

--- bases-node/app.js
+++ bases-node/app.js
@@ -2,9 +2,17 @@ const { crearArchivo } = require("./helpers/multiplicar");
 
 console.clear();
 
-const base = 14;
+const [, , arg3 = 'base=0'] = process.argv;
+const [, base = 0] = arg3.split("=");
+console.log(base);
+
+// for (let index = 0; index < arg3.length; index++) {
+//     console.log(arg3[index]);
+
+// }
+
+// const base = arg3;
 
 crearArchivo(base)
 .then(nombreArchivo => console.log(nombreArchivo, 'ha sido creado'))
 .catch(err => console.log(err))
-
