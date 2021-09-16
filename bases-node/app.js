1351c22a99b0d2cf95f57317678a3e94133e3113
johannpereze <sebas.jsp@gmail.com>
2021-09-15 20:46:11 -0500

    Ahora podemos recibir parametros por consola gracias a yargs

--- bases-node/app.js
+++ bases-node/app.js
@@ -1,16 +1,17 @@
 const { crearArchivo } = require("./helpers/multiplicar");
+const argv = require("yargs").argv;
 
 console.clear();
 
-const [, , arg3 = 'base=0'] = process.argv;
-const [, base = 0] = arg3.split("=");
-console.log(base);
+console.log(process.argv);
+console.log(argv);
+console.log('base: ',argv.base);
 
-// for (let index = 0; index < arg3.length; index++) {
-//     console.log(arg3[index]);
+//Forma vieja
+// const [, , arg3 = 'base=0'] = process.argv;
+// const [, base = 0] = arg3.split("=");
+// console.log(base);
 
-// }
-
-crearArchivo(base)
-.then(nombreArchivo => console.log(nombreArchivo, 'ha sido creado'))
-.catch(err => console.log(err))
+// crearArchivo(base)
+// .then(nombreArchivo => console.log(nombreArchivo, 'ha sido creado'))
+// .catch(err => console.log(err))
